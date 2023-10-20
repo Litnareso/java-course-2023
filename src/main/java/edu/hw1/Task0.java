053413d2976eb32c8f1340792fe4fb378595a980
litnareso <litnareso@protonmail.com>
2023-10-20 03:01:43 +0300

    Add Task0

---
+++ src/main/java/edu/hw1/Task0.java
@@ -0,0 +1,21 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Task0 {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private Task0() {
+    }
+
+    /**
+     * Writes "Привет, мир!" using LOGGER.info().
+     *
+     */
+    public static void hello() {
+        String hello = new String("Привет, мир!");
+
+        LOGGER.info(hello);
+    }
+}
